e9b0e46c0352654a71b3c1425d52dec56efab369
ervz <ervin.mitra@playonsports.com>
2025-11-22 22:05:45 -0500

    return 5

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,14 @@
 fn main() {
     println!("Hello, world!");
     another_function(5);
+    let x = return_5();
+    println!("The value of x is: {x}")
 }
 
 fn another_function(x: i32) {
     println!("The value of x is: {x}");
+}
+
+fn return_5() -> i32 {
+    5
 }
