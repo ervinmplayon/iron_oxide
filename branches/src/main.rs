2f671b384595c084dd251cd8b947c9339e0c4841
ervz <ervin.mitra@playonsports.com>
2025-11-24 13:06:20 -0500

    im a fkin genius

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -6,4 +6,20 @@ fn main() {
     } else {
         println!("condition is false");
     }
+
+    im_a_fkin_genius();
+}
+
+fn im_a_fkin_genius() {
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
 }
