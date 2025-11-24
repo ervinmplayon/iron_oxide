4e9a65d9546ec3b3be3d0d46f66c4fbeff621440
ervz <ervin.mitra@playonsports.com>
2025-11-24 16:07:48 -0500

    add while loop

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -16,9 +16,24 @@ fn main() {
         count += 1;
     }
     println!("end count = {count}");
+    println!("===========================================");
+    println!("WHILE LOOP");
+    while_loopz();
 }
 
 // Loop labels to disambiguate between multiple loops
 // If you have loops within loops, break and continue apply to the innermost loop at that point. You can optionally specify a loop label on a loop
 // that you can then use with break or continue to specify that those keywords apply to the labeled loop instead of the innermost loop. 
 // Loop labels must begin with a single quote.
+
+fn while_loopz() {
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+}
