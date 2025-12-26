38c77008cdabccef80235c28808bd9bdf23aed7c
ervz <ervin.mitra@playonsports.com>
2025-12-25 22:28:38 -0500

    references are immutable by default

--- references/src/main.rs
+++ references/src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
     let s1 = String::from("hello");
     let len = calculate_length(&s1);
-    println!("The length of '{s1}' is {len}")
+    println!("The length of '{s1}' is {len}");
+    let mut x = String::from("hey");
+    println!("x is '{x}'");
+    change(&mut x);
+    println!("x is now '{x}'");
 }
 
 // The issue with the tuple code used in ../ownership/src/main/ calculate_length function is that 
@@ -13,3 +17,7 @@ fn calculate_length(s: &String) -> usize {
     s.len()
 } // Here, s goes out of scope. But because s does not have ownership of 
   // what it refers to, the String is not dropped. 
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
